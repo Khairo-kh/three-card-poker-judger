c4e0dc21a3b5351ef10558d508507ccfe0dafff6
Khairo <khairo.khatib@gmail.com>
2022-04-11 01:46:17 -0400

    refactor: made cards property private in PokerHand class and added getter/setter

--- src/models/hand/PokerHand.ts
+++ src/models/hand/PokerHand.ts
@@ -4,13 +4,22 @@ import { Suit } from '../card/Suits';
 import { Hand } from './Hand';
 
 export class PokerHand implements Hand {
-  public cards: Array<Card>;
+  private _cards: Array<Card>;
 
   constructor(cards: Array<Card>) {
-    this.cards = cards;
+    this._cards = cards;
   }
+
+  get cards() {
+    return this._cards;
+  }
+
+  set cards(cards: Card[]) {
+    this._cards = cards;
+  }
+  
   sortCards(rankCriteria: Rank): Array<Card> {
-    const currentHand = [...this.cards];
+    const currentHand = [...this._cards];
     if (currentHand.length < 3) {
       throw new Error('The hand is empty or have less than 3 cards which is invalid for 3-cards poker!');
     }
@@ -18,14 +27,14 @@ export class PokerHand implements Hand {
     let sortedHand: Array<Card> = [];
     let suitCardsMap: Map<Suit, Card[]> = new Map<Suit, Card[]>();
 
-    this.cards.forEach((card) => {
+    this._cards.forEach((card) => {
       const targetCollection = suitCardsMap.get(card.suit);
       targetCollection === undefined
         ? suitCardsMap.set(card.suit, [card])
         : suitCardsMap.set(card.suit, [...targetCollection, card]);
     });
-      suitCardsMap = new Map<Suit, Card[]>([...suitCardsMap.entries()].sort());
-      
+    suitCardsMap = new Map<Suit, Card[]>([...suitCardsMap.entries()].sort());
+
     console.log('paaaam', suitCardsMap);
 
     suitCardsMap.forEach((group: Card[]) => {
@@ -36,7 +45,7 @@ export class PokerHand implements Hand {
       );
     });
 
-    this.cards = [...sortedHand];
+    this._cards = [...sortedHand];
     return sortedHand;
   }
 }
